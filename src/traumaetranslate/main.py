055dc6b9c82c419b54c93c9fa2656611eac15c9e
Quinlan Pfiffer <qpfiffer@gmail.com>
2013-07-09 01:35:05 -0700

    Cleaner, better code.

--- src/traumaetranslate/main.py
+++ src/traumaetranslate/main.py
@@ -52,10 +52,32 @@ class TraumaeTranslate(Client):
         self.logger.debug('Fetching latest suggestions')
 
     def attempt_english_to_traumae(self, potential_english):
-        pass
+        # Turns positivity(noneness) into ['positivity', 'noneness']
+        broken_up = potential_english.replace(")", "").split("(")
+        translated = reduce(
+            lambda accum, val: accum + ENGLISH_TO_TRAUMAE_ALPHABET.get(val, "__"),
+            broken_up,
+            "")
+        return translated
 
     def attempt_traumae_to_english(self, potential_traumae):
-        pass
+        letters = list()
+        [letters.append(potential_traumae[i:i+2]) for i in range(0, len(potential_traumae), 2)]
+        return reduce(lambda a, v: TRAUMAE_TO_ENGLISH.get(v, "__") + "(" + a + ")", letters[::-1], "")
+
+    def get_suggested_meaning_list(self, words):
+        traumae_api_url = "http://api.xxiivv.com/?key=traumae&cmd=read"
+        request = requests.get(traumae_api_url)
+        json = request.json()
+        to_return = {word:"?" for word in words}
+
+        for s_id in json.keys():
+            # ["pixi","research","Expression","sure","head"]
+            for traumae_word in words:
+                if json[s_id][0] == traumae_word:
+                    to_return[traumae_word] = json[s_id][1]
+
+        return to_return.values()
 
     def get_suggested_meaning(self, traumae_word):
         traumae_api_url = "http://api.xxiivv.com/?key=traumae&cmd=read"
@@ -73,11 +95,7 @@ class TraumaeTranslate(Client):
         to_translate = match.groups()[0]
 
         if "(" in to_translate:
-            # Turns positivity(noneness) into ['positivity', 'noneness']
-            broken_up = to_translate.replace(")", "").split("(")
-            translated = reduce(
-                lambda accum, val: accum + ENGLISH_TO_TRAUMAE_ALPHABET[val],
-                broken_up, "")
+            translated = self.attempt_english_to_traumae(to_translate)
             self.send('{nick}: Traumae translation: {translated}, Suggested meaning: {meaning}'.format(
                 nick=data['nick'],
                 translated=translated,
@@ -87,21 +105,20 @@ class TraumaeTranslate(Client):
 
         else:
             # We're probably dealing with a traumae word.
-            if(len(to_translate) % 2 != 0 or
-                '\'' in to_translate):
-                    # They want me to translate something hard. :(
-                self.send("{nick}: You've given me complex word. I'm not that smart yet.".format(
-                    nick=data['nick']),
-                    data=data)
-                return
-            letters = list()
-            [letters.append(to_translate[i:i+2]) for i in range(0, len(to_translate), 2)]
-            #import ipdb; ipdb.set_trace()
-            translated = reduce(lambda a, v: TRAUMAE_TO_ENGLISH[v] + "(" + a + ")", letters[::-1], "")
+            if " " in to_translate:
+                traumae_words = to_translate.split(" ")
+                words = [self.attempt_traumae_to_english(word) for word in traumae_words]
+                translated = reduce(lambda a,v: v + " " + a, words, "")
+                suggested = self.get_suggested_meaning_list(traumae_words)
+                suggested = reduce(lambda a,v: a + " " + v, suggested[::1], "")
+            else:
+                translated = self.attempt_traumae_to_english(to_translate)
+                suggested = self.get_suggested_meaning(to_translate)
+
             self.send('{nick}: Traumae translation: {translated}, Suggested meaning: {meaning}'.format(
                 nick=data['nick'],
                 translated=translated,
-                meaning=self.get_suggested_meaning(to_translate)),
+                meaning=suggested),
                         data=data)
 
 def main():
